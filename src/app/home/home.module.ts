e119a8a00e9a0ba87cf5f74209b725c8753b5651
= <=>
2022-08-21 21:28:21 +0200

    affichage d'un formFieldControl dans home

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '@shared/shared.module';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [HomeComponent],
   exports: [HomeComponent],
   imports: [
     SharedModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
   ],
 })
 export class HomeModule {
